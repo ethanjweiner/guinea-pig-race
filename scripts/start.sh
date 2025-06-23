f903da48c7c7c5748158f98fe6d26d999075adb5
Ethan Weiner <ethanweiner20@gmail.com>
2025-06-22 21:42:03 -0400

    redirect stdout and stderr

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo Starting Gunicorn
 pkill gunicorn
-poetry run gunicorn race_site.wsgi:application --bind 127.0.0.1:8000
+gunicorn race_site.wsgi:application --bind 127.0.0.1:8000 > /dev/null 2>&1 &
